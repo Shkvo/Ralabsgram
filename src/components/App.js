9fc3c644ff7a88ed0262f31a348cf6a9c09d2875
shkvo <dimashkvo@gmail.com>
2018-01-09 02:34:24 +0200

    Added redux and router; Edited structure of app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
+      <div className="app">
+        
       </div>
     );
   }
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const reducer = (state = {}, action) => {
+  switch(action.type){
+    case 'NONE':
+      return state;
+  }
+}
+
+export default reducer;
