ad12b2b187a4cd4031954b750cf762ac69641383
shkvo <dimashkvo@gmail.com>
2018-01-12 00:26:42 +0200

    Added some files to app structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Header from  './Header';
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        
-      </div>
+      <BrowserRouter>
+        <div className='app'>
+          <Header />
+        </div>
+      </BrowserRouter>
     );
   }
 }
