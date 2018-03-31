b21949f8ebd364d9d895dfb6eb7e6dc88ed12797
shkvo <dimashkvo@gmail.com>
2018-03-31 23:58:01 +0300

    Added basic unit tests for App component

--- src/test/components/App.test.js
+++ src/test/components/App.test.js
@@ -6,29 +6,26 @@ import configureStore from 'redux-mock-store';
 import createSagaMiddleware from 'redux-saga';
 
 const saga = createSagaMiddleware();
-const middlewares = [saga]; 
+const middlewares = [saga];
 const mockStore = configureStore(middlewares);
 
 let wrapper = null;
 
-const store = mockStore(
-  {
-    user: {
-      isLogged: false,
-      access_token: '',
-      info: {},
-      media: []
-    },
-    media: {
-      details: {},
-      comments: [],
-      likes: []
-    },
+const store = mockStore({
+  user: {
+    isLogged: false,
+    access_token: '',
+    info: {},
+    media: []
+  },
+  media: {
+    details: {},
+    comments: [],
+    likes: []
   }
-);
+});
 
 describe('<App /> component:', () => {
-
   beforeEach(() => {
     wrapper = mount(
       <Provider store={store}>
@@ -39,24 +36,40 @@ describe('<App /> component:', () => {
     );
   });
 
-  test('Component renders properly without crashing', () => {
+  test('Renders properly without crashing', () => {
     const app = wrapper.find('App');
-    
+
     expect(app.exists()).toBeTruthy;
-    expect(app.hasClass('app')).toBeTruthy;    
+    expect(app.hasClass('app')).toBeTruthy;
   });
 
-  test('Component renders children properly', () => {
+  test('Renders children properly', () => {
     const app = wrapper.find('App').children();
-    
+
     expect(app.children().length).toEqual(2);
-    expect(app.childAt(0).name()).toEqual('Connect(LeftSideBar)');
+    expect(
+      app
+        .childAt(0)
+        .find('LeftSideBar')
+        .exists()
+    ).toBeTruthy;
+
     expect(app.childAt(1).name()).toEqual('Switch');
-    expect(app.childAt(1).children().length).toEqual(3);
+    expect(app.childAt(1).children().length).toEqual(1);
+    expect(
+      app
+        .childAt(1)
+        .children()
+        .find('Feed')
+        .exists()
+    ).toBeTruthy;
   });
 
-  test('Component has proper props', () => {
-    // expect(wrapper.children().length).toEqual(2);    
+  test('Has proper props', () => {
+    expect(wrapper.find('App').instance().props.accessToken).toEqual('');
   });
-  
+
+  test('Has no state', () => {
+    expect(wrapper.find('App').instance().state).toBeNull;
+  });
 });
