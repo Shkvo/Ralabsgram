d6af748c7ed91110dc7869f2c112dc071c1e9be5
shkvo <dimashkvo@gmail.com>
2018-04-01 00:24:11 +0300

    Added basic tests for Spinner component and edited for PageNotFound

--- src/test/components/404.test.js
+++ src/test/components/404.test.js
@@ -2,12 +2,18 @@ import React from 'react';
 import PageNotFound from '../../components/404';
 
 describe('<PageNotFound /> component:', () => {
+  let wrapper;
 
-  test('Component renders properly without crashing', () => {
-    const wrapper = shallow(<PageNotFound />);
+  beforeEach(() => {
+    wrapper = shallow(<PageNotFound />);
+  });
 
+  test('Renders properly without crashing', () => {
     expect(wrapper.name()).toEqual('h1');
     expect(wrapper.text()).toEqual('404');
   });
 
+  test('Has no props', () => {
+    expect(wrapper.props()).toEqual({ children: '404' });
+  });
 });
--- src/test/components/Spinner.test.js
+++ src/test/components/Spinner.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import PageNotFound from '../../components/404';
+import Spinner from '../../components/Spinner';
 
-describe('<PageNotFound /> component:', () => {
+describe('<Spinner /> component:', () => {
+  let wrapper;
 
-  test('Component renders properly without crashing', () => {
-    const wrapper = shallow(<PageNotFound />);
-    expect(wrapper.name()).toEqual('h1');
+  beforeEach(() => {
+    wrapper = shallow(<Spinner />);
   });
-  
+
+  test('renders properly without crashing', () => {
+    expect(wrapper.name()).toEqual('div');
+    expect(wrapper.hasClass('spinner-wrapper')).toBeTruthy;
+  });
+
+  test('children renders properly', () => {
+    expect(wrapper.children().length).toEqual(1);
+    expect(wrapper.children().name()).toEqual('CircularProgress');
+  });
+
+  test('has proper color prop', () => {
+    expect(wrapper.children().props().color).toEqual('red');
+  });
 });
